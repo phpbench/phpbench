d854e76e31e04a6634952cb0652655040bb7d836
dantleech <dan.t.leech@gmail.com>
2015-05-04 17:28:36 +0200

    Updated bin

--- bin/phpbench.php
+++ bin/phpbench.php
@@ -9,10 +9,24 @@
  * file that was distributed with this source code.
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
+function includeIfExists($file)
+{
+    if (file_exists($file)) {
+        return include $file;
+    }
+}
 
-use PhpBench\Console\Command\BenchRunCommand;
+if ((!includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    fwrite(STDERR,
+        'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL
+    );
+    exit(1);
+}
+
 use Symfony\Component\Console\Application;
+use PhpBench\Console\Command\BenchRunCommand;
 
 $application = new Application();
 $application->add(new BenchRunCommand());
