8d251d9a2f6e4acb043679a20f70338d783b7a76
dantleech <dan.t.leech@gmail.com>
2015-08-05 15:21:21 +0200

    Added outputawatreinterface

---
+++ lib/Console/OutputAwareInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the PHP Bench package
+ *
+ * (c) Daniel Leech <daniel@dantleech.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PhpBench\Console;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+interface OutputAwareInterface
+{
+    public function setOutput(OutputInterface $output);
+}
