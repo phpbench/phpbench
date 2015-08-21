2eca4f2e77e2400dacc617f16de985166bb2915f
dantleech <dan.t.leech@gmail.com>
2015-08-21 10:16:28 +0200

    JSON report configuration

--- lib/Report/Generator/ConsoleTabularGenerator.php
+++ lib/Report/Generator/ConsoleTabularGenerator.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Helper\Table;
 use PhpBench\Console\OutputAwareInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-require_once(__DIR__ . '/Tabular/xpath_functions.php');
+require_once(__DIR__ . '/tabular/xpath_functions.php');
 
 class ConsoleTabularGenerator implements ReportGeneratorInterface, OutputAwareInterface
 {
@@ -67,56 +67,7 @@ class ConsoleTabularGenerator implements ReportGeneratorInterface, OutputAwareIn
             $this->output->writeln($document->saveXML());
         }
 
-        $definition = array(
-            'rows' => array(
-                array(
-                    'cells' => array(
-                        'benchmark' => array(
-                            'expr' => 'class_name(string(ancestor-or-self::benchmark/@class))',
-                        ),
-                        'subject' => array(
-                            'expr' => 'string(ancestor-or-self::subject/@name)',
-                        ),
-                        'group' => array(
-                            'expr' => 'string(ancestor-or-self::group/@name)',
-                        ),
-                        'params' => array(
-                            'expr' => 'parameters_to_json(ancestor-or-self::variant/parameter)',
-                        ),
-                        'memory' => array(
-                            'expr' => 'number(descendant-or-self::iteration/@memory)',
-                        ),
-                        'revs' => array(
-                            'expr' => 'number(descendant-or-self::iteration/@revs)',
-                        ),
-                        'iter' => array(
-                            'expr' => 'count(descendant-or-self::iteration/preceding-sibling::*)',
-                        ),
-                        'time' => array(
-                            'class' => 'microsecond',
-                            'expr' => 'number(descendant-or-self::iteration/@time) div number(sum(descendant-or-self::iteration/@revs))',
-                        ),
-                        'time_net' => array(
-                            'class' => 'microsecond',
-                            'expr' => 'number(descendant-or-self::iteration/@time)',
-                        ),
-                        'rps' => array(
-                            'expr' => '(1000000 div number(descendant-or-self::iteration//@time)) * number(descendant-or-self::iteration/@revs)',
-                        ),
-                        'deviation' => array(
-                            'class' => 'deviation',
-                            'expr' => 'deviation(min(//@time), number(descendant-or-self::iteration/@time))',
-                        ),
-                    ),
-                    'with_query' => '//iteration',
-                ),
-            ),
-            'classes' => array(
-                'microsecond' => array('printf', array('format' => '%s<comment>μs</comment>')),
-                'deviation' => array('printf', array('format' => '%.2f')),
-            ),
-        );
-
+        $definition = json_decode(file_get_contents(__DIR__ . '/tabular/console_default.json'), true);
         $tableDom = $this->tabular->tabulate($document, $definition);
 
         if ($config['debug']) {
