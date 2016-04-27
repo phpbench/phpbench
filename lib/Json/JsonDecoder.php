0273f1990c05624abd38a3d9a014b017ffab5420
dantleech <dan.t.leech@gmail.com>
2016-04-27 15:55:26 +0100

    Non-strict JSON parser

--- lib/Json/JsonDecoder.php
+++ lib/Json/JsonDecoder.php
@@ -47,28 +47,116 @@ class JsonDecoder
     }
 
     /**
-     * Taken from: https://gist.github.com/larruda/967110d74d98c1cd4ee1.
+     * Allow "non-strict" JSON - i.e. if no quotes are provided then try and
+     * add them.
      */
     private function normalize($jsonString)
     {
         if (!is_string($jsonString)) {
             throw new \InvalidArgumentException(sprintf(
-                'Expected a string, got a "%s"',
-                is_object($jsonString) ? get_class($jsonString) : gettype($jsonString)
+                'Expected a string, got "%s"',
+                gettype($jsonString)
             ));
         }
+        $chars = str_split($jsonString);
+        $inRight = $inQuote = $inFakeQuote = false;
+        $fakeQuoteStart = null;
+        $addedStartBracket = false;
 
-        if (substr($jsonString, 0, 1) !== '{') {
-            $jsonString = '{' . $jsonString;
-            $jsonString = $jsonString . '}';
+        if (empty($chars)) {
+            return;
         }
 
-        $jsonString = preg_replace(
-            '{(\s*?\{\s*?|\s*?,\s*?)([\'"])?([\$\.a-zA-Z0-9\[\]\_]+)([\'"])?:}',
-            '\1"\3":',
-            $jsonString
-        );
+        if ($chars[0] !== '{') {
+            array_unshift($chars, '{');
+            $addedStartBracket = true;
+        }
 
-        return $jsonString;
+        for ($index = 0; $index < count($chars); $index++) {
+            $char = $chars[$index];
+            $prevChar = isset($chars[$index - 1]) ? $chars[$index - 1] : null;
+
+            if (!$inQuote && $prevChar == ':') {
+                $inRight = true;
+            }
+
+            if (!$inQuote && preg_match('{\{,}', $char)) {
+                $inRight = false;
+            }
+
+            // detect start of unquoted string
+            if (!$inQuote && preg_match('{[\$a-zA-Z0-9]}', $char)) {
+                array_splice($chars, $index, 0, '"');
+                $fakeQuoteStart = $index;
+                $index++;
+                $inQuote = $inFakeQuote = true;
+                continue;
+            }
+
+            // if we added a "fake" quote, look for the end of the unquoted string
+            if ($inFakeQuote && preg_match('{[\s:\}\],]}', $char)) {
+                // if we are on the left side, then "]" is OK.
+                if (!$inRight && $char === ']') {
+                    continue;
+                }
+
+                if ($inRight) {
+
+                    // extract the right hand value
+                    $string = implode('', array_slice($chars, $fakeQuoteStart + 1, $index - 1 - $fakeQuoteStart));
+
+                    // if it is a number, then we don't quote it
+                    if (is_numeric($string)) {
+                        unset($chars[$fakeQuoteStart]);
+                        $chars = array_values($chars);
+                        $inQuote = $inFakeQuote = false;
+                        $index--;
+                        continue;
+                    }
+
+                    // if it is a boolean, then we don't quote it
+                    if (in_array($string, ['true', 'false'])) {
+                        unset($chars[$fakeQuoteStart]);
+                        $chars = array_values($chars);
+                        $inQuote = $inFakeQuote = false;
+                        $index--;
+                        continue;
+                    }
+                }
+
+                // add the ending quote
+                array_splice($chars, $index, 0, '"');
+                $index++;
+                $inQuote = $inFakeQuote = false;
+                continue;
+            }
+
+            // enter standard quote mode
+            if (!$inQuote && $char === '"') {
+                $inQuote = true;
+                continue;
+            }
+
+            // if we are in quote mode and encounter a closing quote, and the last character
+            // was not the escape character
+            if ($inQuote && $char === '"' && $prevChar !== '\\') {
+                $inQuote = $inFakeQuote = false;
+                continue;
+            }
+        }
+
+        // if we were in a fake quote and hit the end, then add the closing quote
+        if ($inFakeQuote) {
+            $chars[] = '"';
+        }
+
+        $normalized = implode('', $chars);
+
+        // if we added a bracket at the begining, then add one to the end if required
+        if ($addedStartBracket) {
+            $normalized .= '}';
+        }
+
+        return $normalized;
     }
 }
