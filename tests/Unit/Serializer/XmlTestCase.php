a5ffe17b83c6ba2f355ab834fc56b713c5784acc
dantleech <dan.t.leech@gmail.com>
2017-12-21 23:31:41 +0100

    Include timezone in serialized date

--- tests/Unit/Serializer/XmlTestCase.php
+++ tests/Unit/Serializer/XmlTestCase.php
@@ -57,7 +57,7 @@ class XmlTestCase extends TestCase
 
         $this->suiteCollection->getSuites()->willReturn([$this->suite->reveal()]);
         $this->suite->getUuid()->willReturn(1234);
-        $this->suite->getDate()->willReturn(new \DateTime('2015-01-01'));
+        $this->suite->getDate()->willReturn(new \DateTime('2015-01-01T00:00:00+00:00'));
         $this->suite->getContextName()->willReturn('test');
         $this->suite->getConfigPath()->willReturn('/path/to/config.json');
         $this->suite->getEnvInformations()->willReturn([
@@ -153,7 +153,7 @@ class XmlTestCase extends TestCase
                 <<<'EOT'
 <?xml version="1.0"?>
 <phpbench version="PHPBENCH_VERSION">
-  <suite context="test" date="2015-01-01 00:00:00" config-path="/path/to/config.json" uuid="1234">
+  <suite context="test" date="2015-01-01T00:00:00+00:00" config-path="/path/to/config.json" uuid="1234">
     <env>
       <info1 foo="bar"/>
     </env>
@@ -184,7 +184,7 @@ EOT
                 <<<'EOT'
 <?xml version="1.0"?>
 <phpbench version="PHPBENCH_VERSION">
-  <suite context="test" date="2015-01-01 00:00:00" config-path="/path/to/config.json" uuid="1234">
+  <suite context="test" date="2015-01-01T00:00:00+00:00" config-path="/path/to/config.json" uuid="1234">
     <env>
       <info1 foo="bar"/>
     </env>
@@ -207,7 +207,7 @@ EOT
                 <<<'EOT'
 <?xml version="1.0"?>
 <phpbench version="PHPBENCH_VERSION">
-  <suite context="test" date="2015-01-01 00:00:00" config-path="/path/to/config.json" uuid="1234">
+  <suite context="test" date="2015-01-01T00:00:00+00:00" config-path="/path/to/config.json" uuid="1234">
     <env>
       <info1 foo="bar"/>
     </env>
