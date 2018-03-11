f983fba0ca1e38318d76c96c37970edb32281e2a
dantleech <dan.t.leech@gmail.com>
2018-03-11 09:59:02 +0100

    Reenable build

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -5,15 +5,15 @@ openssl aes-256-cbc -K $encrypted_d58d55177063_key -iv $encrypted_d58d55177063_i
 
 tar xvf .travis/secrets.tar -C .travis
 
-#composer require "doctrine/dbal" "~2.5"  --no-update
-#composer require "padraic/phar-updater" "^1.0" --no-update
+composer require "doctrine/dbal" "~2.5"  --no-update
+composer require "padraic/phar-updater" "^1.0" --no-update
 
-#curl -LSs https://box-project.github.io/box2/installer.php | php
+curl -LSs https://box-project.github.io/box2/installer.php | php
 
 rm -Rf ./vendor
-#composer install --no-dev -o
+composer install --no-dev -o
 
-#box.phar build
+box.phar build
 echo "<?php echo 'Testing build;'" > phpbench.phar
 
 sha1sum phpbench.phar > phpbench.phar.version
