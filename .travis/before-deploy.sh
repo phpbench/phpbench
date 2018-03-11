3da6d483d5bdd8c1f536b857035f63f73dc4b671
dantleech <dan.t.leech@gmail.com>
2018-03-11 10:24:38 +0100

    Include path

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+export PATH=.:$PATH
+
 openssl aes-256-cbc -K $encrypted_d58d55177063_key -iv $encrypted_d58d55177063_iv -in .travis/secrets.tar.enc -out .travis/secrets.tar -d
 
 tar xvf .travis/secrets.tar -C .travis
