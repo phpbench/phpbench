963735a94946d0301c85f99701a2f796774f07dc
dantleech <dan.t.leech@gmail.com>
2018-06-16 15:18:18 +0200

    Benchmark File Iterator test

--- tests/Unit/Runner/BenchmarkFileIteratorTest.php
+++ tests/Unit/Runner/BenchmarkFileIteratorTest.php
@@ -5,6 +5,8 @@ namespace PhpBench\Tests\Unit\Runner;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use PhpBench\Runner\BenchmarkFileIterator;
+use PhpBench\Tests\Util\Workspace;
+use SplFileInfo;
 use function iterator_to_array;
 
 class BenchmarkFileIteratorTest extends TestCase
@@ -14,6 +16,45 @@ class BenchmarkFileIteratorTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         $iterator = $this->create(__DIR__ . '/not-existing-file.php');
         iterator_to_array($iterator);
+    }
+
+    public function testIteratesOverFilesInDirectory()
+    {
+        $workspace = Workspace::create()->init();
+        $workspace->createFile('foobar/hello.php');
+        $workspace->createFile('foobar/barfoo/hello.php');
+        $workspace->createFile('foobar/goodbye.php');
+        $workspace->createFile('goodbye.php');
+
+        $iterator = $this->create($workspace->path('/foobar'));
+        $files = array_map(function (SplFileInfo $file) {
+            return $file->getPathname();
+        }, iterator_to_array($iterator));
+
+        $this->assertEquals([
+            $workspace->path('foobar/hello.php'),
+            $workspace->path('foobar/barfoo/hello.php'),
+            $workspace->path('foobar/goodbye.php'),
+        ], $files);
+    }
+
+    public function testReturnsSingleFileIfAbsolutePathProvided()
+    {
+        $workspace = Workspace::create()->init();
+        $workspace->createFile('hello.php');
+        $workspace->createFile('foobar/barfoo/hello.php');
+        $workspace->createFile('foobar/goodbye.php');
+        $workspace->createFile('goodbye.php');
+
+        $iterator = $this->create($workspace->path('hello.php'));
+        $files = array_map(function (SplFileInfo $file) {
+            return $file->getPathname();
+        }, iterator_to_array($iterator));
+
+        $this->assertCount(1, $files);
+        $this->assertEquals([
+            $workspace->path('hello.php'),
+        ], $files);
     }
 
     private function create(string $path)
--- tests/Util/Workspace.php
+++ tests/Util/Workspace.php
@@ -13,9 +13,38 @@
 namespace PhpBench\Tests\Util;
 
 use Symfony\Component\Filesystem\Filesystem;
+use Webmozart\PathUtil\Path;
 
 class Workspace
 {
+    public static function create(): self
+    {
+        return new Workspace();
+    }
+
+    public function init(): self
+    {
+        self::initWorkspace();
+        return $this;
+    }
+
+    public function path(string $subPath = '/')
+    {
+        return Path::join(self::getWorkspacePath(), $subPath);
+    }
+
+    public function createFile(string $path, $contents = '')
+    {
+        $path = $this->path($path);
+        $directory = dirname($path);
+
+        if ('.' !== $directory && !file_exists($directory)) {
+            mkdir($directory, 0777, true);
+        }
+
+        file_put_contents($path, $contents);
+    }
+
     public static function getWorkspacePath()
     {
         $path = sys_get_temp_dir() . '/phpbench_test_workspace';
