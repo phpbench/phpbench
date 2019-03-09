501f8aa7ad8c3e2f5edb0cdafc7df0cdf6d85969
Daniel Leech <dan.t.leech@gmail.com>
2019-03-09 19:47:58 +0000

    only deploy the PHAR

---
+++ .travis/before-deploy-gh-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# set -x
+
+export PATH=.:$PATH
+
+# Travis won't deploy ignored files
+sed -ie '/^\/phpbench.phar*/d'  .gitignore
+
+# Install PHAR dependencies and remove strip other dev deps.
+rm -Rf ./vendor
+rm -f composer.lock
+composer require "doctrine/dbal" "~2.5"  --no-update
+composer install --no-dev -o
+
+# Build the PHAR
+curl -LSs https://box-project.github.io/box2/installer.php | php
+box.phar build -c box.json.gh-release
+ls
+
+echo "Done pre-deploy build"
