8dc3440d0f83fec59b089fcae3c61f13b5b4593d
Daniel Leech <dan.t.leech@gmail.com>
2020-08-23 18:51:24 +0100

    Fix 7.2 compat

--- lib/Extension/CoreExtension.php
+++ lib/Extension/CoreExtension.php
@@ -288,7 +288,7 @@ class CoreExtension implements ExtensionInterface
         $container->register('console.command.handler.report', function (Container $container) {
             return new ReportHandler(
                 $container->get('report.manager'),
-                $container->get('console.command.handler.suite_collection'),
+                $container->get('console.command.handler.suite_collection')
             );
         });
 
