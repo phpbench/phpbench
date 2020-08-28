f1e4c1ce78c9d6425fbfeb5921f508e9f2d2500b
Daniel Leech <dan.t.leech@gmail.com>
2020-08-28 22:33:59 +0100

    Added expression parser bench

---
+++ tests/Benchmark/ExpresionParserBench.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace PhpBench\Tests\Benchmark;
+
+use Generator;
+use PhpBench\Assertion\ExpressionEvaluatorFactory;
+use PhpBench\Assertion\ExpressionParser;
+
+/**
+ * @Revs(10)
+ * @Iterations(3)
+ * @BeforeMethods({"setUp"})
+ * @OutputTimeUnit("milliseconds")
+ */
+class ExpresionParserBench
+{
+    /**
+     * @var ExpressionParser
+     */
+    private $parser;
+
+    public function setUp(): void
+    {
+        $this->parser = new ExpressionParser();
+    }
+
+    /**
+     * @ParamProviders({"provideExpressions"})
+     * @param array<mixed> $params
+     */
+    public function benchEvaluate(array $params): void
+    {
+        $this->parser->parse($params['expr']);
+    }
+
+    /**
+     * @return Generator<mixed>
+     */
+    public function provideExpressions(): Generator
+    {
+        yield '10 seconds within 10% of 10 seconds' => [
+            'expr' => '10 seconds within 10% of 10 seconds',
+        ];
+
+        yield '10 seconds within 10 seconds of 10 seconds' => [
+            'expr' => '10 seconds within 10 seconds of 10 seconds',
+        ];
+
+        yield '10 seconds within 10 seconds of 10 seconds +/- 10 seconds' => [
+            'expr' => '10 seconds within 10 seconds of 10 seconds +/- 10 seconds',
+        ];
+
+        yield '10 seconds < 10 seconds' => [
+            'expr' => '10 seconds < 10 seconds',
+        ];
+
+        yield '10 seconds < 10 seconds +/- 10 seconds' => [
+            'expr' => '10 seconds < 10 seconds +/- 10 seconds',
+        ];
+    }
+}
