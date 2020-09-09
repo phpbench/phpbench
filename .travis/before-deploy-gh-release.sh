aee1a3fdbcb50bbdae974b4df071e6e1d6731a6f
Daniel Leech <dan.t.leech@gmail.com>
2020-09-09 20:32:03 +0100

    Update box

--- .travis/before-deploy-gh-release.sh
+++ .travis/before-deploy-gh-release.sh
@@ -9,11 +9,9 @@ sed -ie '/^\/phpbench.phar*/d'  .gitignore
 # Install PHAR dependencies and remove strip other dev deps.
 rm -Rf ./vendor
 rm -f composer.lock
-composer require "doctrine/dbal" "~2.5"  --no-update
 composer install --no-dev -o
 
-# Build the PHAR
-wget https://github.com/humbug/box/releases/download/3.5.1/box.phar
+wget https://github.com/box-project/box/releases/download/3.8.5/box.phar
 php box.phar compile -c box.json.gh-release
 
 echo "Done pre-deploy build"
