5b0582270b8c5dfe30168ed549760b3eaceae72a
KEINOS <github@keinos.com>
2020-12-22 17:24:21 +0900

    fix: SC2086 and SC2154 error of shellcheck (#737)
    - SC2086: Double quote to prevent globbing and word splitting.
    - SC2154: Throw error if the referenced variable is  not assigned.

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -7,7 +7,12 @@ export PATH=.:$PATH
 sed -ie '/^\/phpbench.phar*/d'  .gitignore
 
 # Extract the private key (needed to sign the PHAR)
-openssl aes-256-cbc -K $encrypted_d58d55177063_key -iv $encrypted_d58d55177063_iv -in .travis/secrets.tar.enc -out .travis/secrets.tar -d
+openssl aes-256-cbc \
+    -K "${encrypted_d58d55177063_key:?'Encrypted key not assigned'}" \
+    -iv "${encrypted_d58d55177063_iv:?'Encrypted iv not assigned'}" \
+    -in .travis/secrets.tar.enc \
+    -out .travis/secrets.tar \
+    -d
 tar xvf .travis/secrets.tar -C .travis
 
 # Install PHAR dependencies and remove strip other dev deps.
