34d397499baeff4482dcd895161f13039207ddbe
My Name <test@example.com>
2021-02-16 22:58:15 +0000

    Added option to paas params to eval command

--- lib/Console/Command/EvaluateCommand.php
+++ lib/Console/Command/EvaluateCommand.php
@@ -8,13 +8,19 @@ use PhpBench\Assertion\ExpressionParser;
 use PhpBench\Expression\Evaluator;
 use PhpBench\Expression\Parser;
 use PhpBench\Expression\ParserFactory;
+use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use function json_last_error_msg;
 
 class EvaluateCommand extends Command
 {
+    const ARG_EXPR = 'expr';
+    const ARG_PARAMS = 'params';
+
+
     /**
      * @var Lexer
      */
@@ -45,21 +51,43 @@ class EvaluateCommand extends Command
     {
         $this->setName('eval');
         $this->setDescription('Evaluate an expression with the PHPBench expression language');
-        $this->addArgument('expr', InputArgument::REQUIRED);
+        $this->addArgument(self::ARG_EXPR, InputArgument::REQUIRED);
+        $this->addArgument(self::ARG_PARAMS, InputArgument::OPTIONAL, 'JSON encoded parameters');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $expr = $input->getArgument('expr');
+        $expr = $input->getArgument(self::ARG_EXPR);
         assert(is_string($expr));
+        $params = $input->getArgument(self::ARG_PARAMS);
+        assert(is_string($params) || is_null($params));
 
         $node = $this->parser->parse($this->lexer->lex($expr));
         $output->writeln((string)json_encode(
             $this->evaluator->evaluate(
-                $node
+                $node,
+                $this->resolveParams($params)
             )
         ));
 
         return 0;
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    private function resolveParams(?string $params): array
+    {
+        if (null === $params) {
+            return [];
+        }
+
+        if (null === $params = json_decode($params, true)) {
+            throw new RuntimeException(sprintf(
+                'Could not decode JSON: %s',
+                json_last_error_msg()
+            ));
+        }
+        return $params;
     }
 }
