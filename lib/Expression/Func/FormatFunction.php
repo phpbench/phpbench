54d101f483f08bd81e899ac6b350b0009bda925c
Daniel Leech <dan.t.leech@gmail.com>
2021-03-04 22:11:34 +0000

    Function error test

--- lib/Expression/Func/FormatFunction.php
+++ lib/Expression/Func/FormatFunction.php
@@ -2,6 +2,9 @@
 
 namespace PhpBench\Expression\Func;
 
+use function error_clear_last;
+use RuntimeException;
+
 final class FormatFunction
 {
     /**
@@ -11,7 +14,18 @@ final class FormatFunction
      */
     public function __invoke(string $format, ...$values)
     {
+        error_clear_last();
         /** @phpstan-ignore-next-line */
-        return sprintf($format, ...$values);
+        $formatted = @sprintf($format, ...$values);
+
+        if (!is_string($formatted)) {
+            $error = error_get_last();
+            $message = isset($error['message']) ? $error['message'] : 'could not format string';
+            $message = str_replace('sprintf', 'format', $message);
+
+            throw new RuntimeException($message);
+        }
+
+        return $formatted;
     }
 }
--- tests/Unit/Expression/Func/FormatFunctionTest.php
+++ tests/Unit/Expression/Func/FormatFunctionTest.php
@@ -4,13 +4,23 @@ namespace PhpBench\Tests\Unit\Expression\Func;
 
 use PhpBench\Expression\Func\FormatFunction;
 use PhpBench\Tests\Unit\Expression\FunctionTestCase;
+use RuntimeException;
 
 class FormatFunctionTest extends FunctionTestCase
 {
-    public function testEval(): void
+    public function testFormat(): void
     {
         self::assertEquals('2 foo 6', $this->eval(
             new FormatFunction(), '%s %s %s', 2, 'foo', 6
+        ));
+    }
+
+    public function testTooManyPlaceholders(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('format()');
+        self::assertEquals('2 foo 6', $this->eval(
+            new FormatFunction(), '%s %s %s %s', 2, 'foo', 6
         ));
     }
 }
