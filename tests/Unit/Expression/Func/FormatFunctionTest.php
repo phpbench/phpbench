93ba2c0d1268d6db8400c2a3a262eafb55eff1e9
Daniel Leech <dan.t.leech@gmail.com>
2021-03-04 22:30:35 +0000

    Support PHP 8

--- tests/Unit/Expression/Func/FormatFunctionTest.php
+++ tests/Unit/Expression/Func/FormatFunctionTest.php
@@ -2,9 +2,11 @@
 
 namespace PhpBench\Tests\Unit\Expression\Func;
 
+use ArgumentCountError;
 use PhpBench\Expression\Func\FormatFunction;
 use PhpBench\Tests\Unit\Expression\FunctionTestCase;
 use RuntimeException;
+use function phpversion;
 
 class FormatFunctionTest extends FunctionTestCase
 {
@@ -17,8 +19,12 @@ class FormatFunctionTest extends FunctionTestCase
 
     public function testTooManyPlaceholders(): void
     {
-        $this->expectException(RuntimeException::class);
-        $this->expectExceptionMessage('format()');
+        if (PHP_VERSION_ID < 80000) {
+            $this->expectException(RuntimeException::class);
+            $this->expectExceptionMessage('format()');
+        } else {
+            $this->expectException(ArgumentCountError::class);
+        }
         self::assertEquals('2 foo 6', $this->eval(
             new FormatFunction(), '%s %s %s %s', 2, 'foo', 6
         ));
