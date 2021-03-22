6adbba729541b2e9163bbe4d04cad6d926a8c67e
Daniel Leech <dan.t.leech@gmail.com>
2021-03-22 09:13:57 +0000

    Implemented coalesce function

--- lib/Expression/Func/CoalesceFunction.php
+++ lib/Expression/Func/CoalesceFunction.php
@@ -2,6 +2,7 @@
 
 namespace PhpBench\Expression\Func;
 
+use PhpBench\Expression\Ast\NullNode;
 use PhpBench\Expression\Ast\PhpValue;
 use PhpBench\Expression\Ast\StringNode;
 
@@ -9,5 +10,13 @@ final class CoalesceFunction
 {
     public function __invoke(PhpValue ...$values): PhpValue
     {
+        foreach ($values as $value) {
+            if ($value instanceof NullNode) {
+                continue;
+            }
+            return $value;
+        }
+
+        return new NullNode();
     }
 }
--- tests/Unit/Expression/Func/CoalesceFunctionTest.php
+++ tests/Unit/Expression/Func/CoalesceFunctionTest.php
@@ -12,9 +12,25 @@ class CoalesceFunctionTest extends FunctionTestCase
 {
     public function testCoalesce(): void
     {
-        self::assertEquals('10', $this->eval(
+        self::assertEquals(10, $this->eval(
             new CoalesceFunction(),
             '10, null'
+        )->value());
+        self::assertEquals(null, $this->eval(
+            new CoalesceFunction(),
+            'null, null'
+        )->value());
+        self::assertEquals(null, $this->eval(
+            new CoalesceFunction(),
+            ''
+        )->value());
+        self::assertEquals(null, $this->eval(
+            new CoalesceFunction(),
+            'null'
+        )->value());
+        self::assertEquals(12, $this->eval(
+            new CoalesceFunction(),
+            'null,12,null'
         )->value());
     }
 }
