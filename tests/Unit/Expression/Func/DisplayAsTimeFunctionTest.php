a190a1d206006f6c8041e1857cd591d266c57c98
Daniel Leech <dan.t.leech@gmail.com>
2021-03-28 10:25:46 +0100

    DisplayAsTime supports NULL and defaults to microseconds

--- tests/Unit/Expression/Func/DisplayAsTimeFunctionTest.php
+++ tests/Unit/Expression/Func/DisplayAsTimeFunctionTest.php
@@ -6,6 +6,7 @@ use PhpBench\Expression\Ast\DisplayAsTimeNode;
 use PhpBench\Expression\Ast\IntegerNode;
 use PhpBench\Expression\Ast\StringNode;
 use PhpBench\Expression\Ast\UnitNode;
+use PhpBench\Expression\Exception\EvaluationError;
 use PhpBench\Expression\Func\DisplayAsTimeFunction;
 use PhpBench\Tests\Unit\Expression\FunctionTestCase;
 
@@ -33,5 +34,25 @@ class DisplayAsTimeFunctionTest extends FunctionTestCase
             new DisplayAsTimeFunction(),
             '7, "milliseconds"'
         ));
+    }
+
+    public function testDisplayWithNullUnit(): void
+    {
+        self::assertEquals(new DisplayAsTimeNode(
+            new IntegerNode(7),
+            new UnitNode(new StringNode("microseconds"))
+        ), $this->eval(
+            new DisplayAsTimeFunction(),
+            '7, null'
+        ));
+    }
+
+    public function testDisplayWithInvalidType(): void
+    {
+        $this->expectException(EvaluationError::class);
+        $this->eval(
+            new DisplayAsTimeFunction(),
+            '7, 10'
+        );
     }
 }
