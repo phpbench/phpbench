0409ed745f860dc34b60a166ea75719d09727605
Daniel Leech <dan.t.leech@gmail.com>
2021-04-06 15:18:11 +0100

    Use workspace in phpbench test case

--- tests/Unit/Executor/Benchmark/LocalExecutorTest.php
+++ tests/Unit/Executor/Benchmark/LocalExecutorTest.php
@@ -5,6 +5,7 @@ namespace PhpBench\Tests\Unit\Executor\Benchmark;
 use PhpBench\Executor\Benchmark\LocalExecutor;
 use PhpBench\Executor\BenchmarkExecutorInterface;
 use PhpBench\Executor\ExecutionResults;
+use PhpBench\Registry\Config;
 
 class LocalExecutorTest extends AbstractExecutorTestCase
 {
@@ -16,5 +17,21 @@ class LocalExecutorTest extends AbstractExecutorTestCase
     protected function assertExecute(ExecutionResults $results): void
     {
         self::assertCount(1, $results);
+    }
+
+
+    public function testWithBootstrap(): void
+    {
+        $executor = new LocalExecutor();
+        $results = $this->executor->execute(
+            $this->buildContext([
+                'timeOut' => 0.0,
+                'methodName' => 'doSomething',
+                'parameterSetName' => 'one',
+                'revolutions' => 10,
+                'warmup' => 1,
+            ]),
+            new Config('test', [])
+        );
     }
 }
