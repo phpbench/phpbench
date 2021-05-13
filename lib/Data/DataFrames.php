fcee41faf820e5a3a5a27c83611af7cac0507bcf
Daniel Leech <dan.t.leech@gmail.com>
2021-05-13 21:50:41 +0100

    Remove unused combine() function

--- lib/Data/DataFrames.php
+++ lib/Data/DataFrames.php
@@ -26,31 +26,6 @@ class DataFrames implements IteratorAggregate
         return new ArrayIterator($this->dataFrames);
     }
 
-    public function combine(): DataFrame
-    {
-        $rows = [];
-        $columns = null;
-
-        foreach ($this->dataFrames as $dataFrame) {
-            if ($columns !== null && $columns !== $dataFrame->columnNames()) {
-                throw new RuntimeException(sprintf(
-                    'DataFrame with columns "%s" does not match initial data frame with columns "%s"',
-                    implode('", "', $dataFrame->columnNames()), implode('", "', $columns)
-                ));
-            }
-
-            if (null === $columns) {
-                $columns = $dataFrame->columnNames();
-            }
-
-            foreach ($dataFrame as $row) {
-                $rows[] = $row;
-            }
-        }
-
-        return new DataFrame($rows, $columns ?? []);
-    }
-
     public function toArray(): array
     {
         return array_map(function (DataFrame $frame) {
