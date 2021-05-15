b6f9bd0f9dc4f28ad099a8df0ba9115219b6b407
Daniel Leech <dan.t.leech@gmail.com>
2021-05-15 16:06:49 +0100

    Added commentary

--- lib/Report/Generator/ExpressionGenerator.php
+++ lib/Report/Generator/ExpressionGenerator.php
@@ -157,11 +157,20 @@ EOT
         $expressionMap = $this->resolveExpressionMap($config);
         $baselineExpressionMap = $this->resolveBaselineExpressionMap($config, array_keys($expressionMap));
 
+        // transform the suite into a data frame
         $frame = $this->transformer->suiteToTable($collection, $config[self::PARAM_INCLUDE_BASELINE]);
+        // parition the data frame into data frames grouped by aggregates
         $frames = $frame->partition($config[self::PARAM_AGGREGATE]);
+
+        // evaluate the aggregated values with the expression language - at
+        // this point the data frame is coverted to an array with Node
+        // instances rather than scalar values
         $table = iterator_to_array($this->evaluate($frame, $frames, $expressionMap, $baselineExpressionMap));
+
+        // split the evaluated tables by "break" for display
         $tables = $this->partition($table, $config[self::PARAM_BREAK]);
 
+        // convert the array into Table instances for rendering
         return $this->generateReports($tables, $config);
     }
 
