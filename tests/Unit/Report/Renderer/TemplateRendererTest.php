78fc6c03d4d016cb9f62e674598398b9f6850772
Daniel Leech <dan.t.leech@gmail.com>
2021-05-19 21:04:46 +0100

    Added report templates

---
+++ tests/Unit/Report/Renderer/TemplateRendererTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the PHPBench package
+ *
+ * (c) Daniel Leech <daniel@dantleech.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace PhpBench\Tests\Unit\Report\Renderer;
+
+use Generator;
+use PhpBench\Expression\Printer;
+use PhpBench\Report\Renderer\ConsoleRenderer;
+use PhpBench\Report\RendererInterface;
+use PhpBench\Report\Renderer\TemplateRenderer;
+use PhpBench\Template\ObjectRenderer;
+use PhpBench\Tests\Util\Approval;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class TemplateRendererTest extends AbstractRendererCase
+{
+    /**
+     * @var BufferedOutput
+     */
+    private $output;
+
+    protected function setUp(): void
+    {
+        $this->workspace()->reset();
+    }
+
+    protected function getRenderer(): RendererInterface
+    {
+        return new TemplateRenderer(
+            $this->container()->get(ObjectRenderer::class),
+            $this->workspace()->path()
+        );
+    }
+
+    /**
+     * @dataProvider provideRender
+     */
+    public function testRender(string $path): void
+    {
+        $approval = Approval::create($path, 2);
+
+        $this->renderReport($this->reports(), $approval->getConfig(0));
+
+        self::assertFileExists($this->workspace()->path('index.html'));
+        $approval->approve($this->workspace()->getContents('index.html'));
+    }
+
+    public function provideRender(): Generator
+    {
+        foreach (glob(sprintf('%s/%s/*', __DIR__, 'template')) as $path) {
+            yield [$path];
+        }
+    }
+}
