d3d734697726d21d36ef5c23bc6b3efb38fdfb6b
Daniel Leech <dan.t.leech@gmail.com>
2021-07-18 09:56:01 +0100

    update variable parselet test

--- tests/Unit/Expression/Parselet/VariableParseletTest.php
+++ tests/Unit/Expression/Parselet/VariableParseletTest.php
@@ -3,11 +3,6 @@
 namespace PhpBench\Tests\Unit\Expression\Parselet;
 
 use Generator;
-use PhpBench\Expression\Ast\ComparisonNode;
-use PhpBench\Expression\Ast\IntegerNode;
-use PhpBench\Expression\Ast\NullSafeNode;
-use PhpBench\Expression\Ast\ParameterNode;
-use PhpBench\Expression\Ast\StringNode;
 use PhpBench\Expression\Ast\VariableNode;
 use PhpBench\Tests\Unit\Expression\ParseletTestCase;
 
@@ -18,72 +13,9 @@ class VariableParseletTest extends ParseletTestCase
      */
     public function provideParse(): Generator
     {
-        return;
-
-        yield 'property' => [
-            'foo.bar',
-            new ParameterNode([
-                new VariableNode('foo'),
-                new VariableNode('bar')
-            ])
-        ];
-
-        yield 'field 1' => [
-            'foo["bar"]',
-            new ParameterNode([
-                new VariableNode('foo'),
-                new StringNode('bar')
-            ])
-        ];
-
-        yield 'field 2' => [
-            'foo["bar"]["bar"]',
-            new ParameterNode([
-                new VariableNode('foo'),
-                new StringNode('bar'),
-                new StringNode('bar'),
-            ])
-        ];
-
-        yield 'field and property' => [
-            'foo["bar"]["bar"].baz',
-            new ParameterNode([
-                new VariableNode('foo'),
-                new StringNode('bar'),
-                new StringNode('bar'),
-                new VariableNode('baz')
-            ])
-        ];
-
-        yield 'expression' => [
-            'foo[bar <= 10]["bar"].baz',
-            new ParameterNode([
-                new VariableNode('foo'),
-                new ComparisonNode(
-                    new ParameterNode([new VariableNode('bar')]),
-                    '<=',
-                    new IntegerNode(10)
-                ),
-                new StringNode('bar'),
-                new VariableNode('baz')
-            ])
-        ];
-
-        yield 'null safe property' => [
-            'foo?.bar',
-            new ParameterNode([
-                new VariableNode('foo'),
-                new NullSafeNode(new VariableNode('bar'))
-            ])
-        ];
-
-        yield 'null safe field 2' => [
-            'foo["bar"]?["bar"]',
-            new ParameterNode([
-                new VariableNode('foo'),
-                new StringNode('bar'),
-                new NullSafeNode(new StringNode('bar')),
-            ])
+        yield 'variable' => [
+            'foo',
+            new VariableNode('foo')
         ];
     }
 
@@ -92,42 +24,10 @@ class VariableParseletTest extends ParseletTestCase
      */
     public function provideEvaluate(): Generator
     {
-        return;
-
         yield [
-            'foo.bar',
-            ['foo' => ['bar' => 12]],
-            '12'
-        ];
-
-        yield [
-            'foo.bar',
-            ['foo' => ['bar' => 'foo']],
-            'foo'
-        ];
-
-        yield [
-            'foo.bar',
-            ['foo' => ['bar' => null]],
-            'null'
-        ];
-
-        yield 'null safe 1' => [
-            'foo?.bar',
-            ['foo' => ['baz' => null]],
-            'null'
-        ];
-
-        yield 'null safe 2' => [
-            'foo?.bar?.foo',
-            ['foo' => ['baz' => null]],
-            'null'
-        ];
-
-        yield 'null safe 3' => [
-            'foo?.bar?.foo',
-            ['foo' => ['bar' => null]],
-            'null'
+            'foo',
+            ['foo' => [12]],
+            '[12]'
         ];
     }
 
