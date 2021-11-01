c41e4acfa883884daf4f383583acb30480cd9a31
Daniel Leech <dan.t.leech@gmail.com>
2021-11-01 19:37:36 +0100

    Add comment

--- lib/Model/Tag.php
+++ lib/Model/Tag.php
@@ -25,10 +25,13 @@ final class Tag
 
     public function __construct(string $tag)
     {
+        // be restrictive with tag chars as:
+        //
+        // - `-` is reserved currently (e.g. my-tag-5 will show the 5th instance of my-tag)
+        // - we don't know how tags will be used in storage implementations
         if (!preg_match(sprintf('/^%s$/', self::REGEX_PATTERN), $tag)) {
             throw new InvalidTagException(sprintf(
                 'Tag must be non-empty string of alphanumeric characters, "." or "_". Got "%s"',
-
                 $tag
             ));
         }
