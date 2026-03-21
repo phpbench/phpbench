3b230611a99f0c23425dbe94d78dac1fdc9b4be9
dantleech <dan.t.leech@gmail.com>
2026-03-21 16:44:34 +0000

    1.6.0 (#1142)
    * Support loose boolean values
    
    * Bump to 1.6.0

--- lib/Expression/Func/IfFunction.php
+++ lib/Expression/Func/IfFunction.php
@@ -2,14 +2,13 @@
 
 namespace PhpBench\Expression\Func;
 
-use PhpBench\Expression\Ast\BooleanNode;
 use PhpBench\Expression\Ast\PhpValue;
 
 final class IfFunction
 {
-    public function __invoke(BooleanNode $condition, PhpValue $trueVal, PhpValue $falseVal): PhpValue
+    public function __invoke(PhpValue $condition, PhpValue $trueVal, PhpValue $falseVal): PhpValue
     {
-        if ($condition->value() === true) {
+        if ((bool)$condition->value() === true) {
             return $trueVal;
         }
 
--- tests/Unit/Expression/Func/IfFunctionTest.php
+++ tests/Unit/Expression/Func/IfFunctionTest.php
@@ -11,6 +11,10 @@ class IfFunctionTest extends FunctionTestCase
     #[TestWith(['true, "foo", 42', 'foo'])]
     #[TestWith(['false, "foo", 42', 42])]
     #[TestWith(['true, [1,2], 42', [1,2]])]
+    #[TestWith(['"true", true, false', true])]
+    #[TestWith(['"", true, false', false])]
+    #[TestWith(['0, true, false', false])]
+    #[TestWith(['1, true, false', true])]
     public function testFormat(string $paramExpr, mixed $expected): void
     {
         self::assertEquals($expected, $this->eval(
